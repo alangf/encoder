f04194900a2137202cdea2f16809c6434803da4e
Alan Gomes <alangf@gmail.com>
2022-08-23 00:14:40 -0400

    refactor input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,32 +1,22 @@
 import {
-  ChangeEvent,
   InputHTMLAttributes,
   TextareaHTMLAttributes,
+  FC
 } from 'react';
 
-type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+type Props = InputHTMLAttributes<HTMLInputElement> & 
+  TextareaHTMLAttributes<HTMLTextAreaElement> & {
   type: string;
 };
 
-type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
-  type: string;
-};
+const classes = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2"
 
-const Input: React.FC<Props> = (props) => {
-  const { value, children, ...textareaProps } = props;
+const Input: FC<Props> = (props: Props) => {
+  const className = `${classes} ${props.className}`.trim()
 
-  return props.type === 'textarea' ? (
-    <textarea {...textareaProps}>{value}</textarea>
-  ) : (
-    <input {...props} />
-  );
+  return props.type === 'textarea'
+    ? <textarea {...props}  className={className} />
+    : <input {...props} className={className} />
 };
 
 export default Input;
-
-<input
-  name="newName"
-  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2"
-  value={newName}
-  onChange={(e) => setNewName(e.target.value)}
-/>;
